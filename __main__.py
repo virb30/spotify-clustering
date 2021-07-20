02346dfa136dffb323acdae538087369536b8b41
Vinicius Boscoa <virb30@gmail.com>
2021-07-19 22:09:59 -0300

    add dataset do git

--- __main__.py
+++ __main__.py
@@ -6,5 +6,5 @@ client_secret = os.environ.get('SPOTIFY_CLIENT_SECRET', '')
 
 if __name__ == '__main__':
     spotify_api = SpotifyApi(client_id, client_secret)
-    df = spotify_api.search_tracks('year:2021', size=1000)
+    df = spotify_api.search_tracks('year:2020', size=1000)
     df.to_csv(os.path.join('spotify_data.csv'))
